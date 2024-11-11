76ddba5a77f73f48fe91bbddffdcb7d0f36054cb
Dieguito-py <diegoBernardi@protonmail.com>
2024-11-11 15:56:31 -0300

    Add JWT auth

--- backend/src/main/java/com/example/backend/providers/JWTProvider.java
+++ backend/src/main/java/com/example/backend/providers/JWTProvider.java
@@ -3,24 +3,31 @@ package com.example.backend.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class JWTProvider {
-
-    @Value("${jwt.secret}")
-    private String secret;
+    @Value("${security.token.secret}")
+    private String secretKey;
 
     public String validateToken(String token) {
+        token = token.replace("Bearer ", "");
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
         try {
-            // Check if the token is in the correct format
-            JWT.require(Algorithm.HMAC256(secret)).build().verify(token);
-            return JWT.decode(token).getSubject();
-        } catch (JWTDecodeException | SignatureVerificationException e) {
-            // Log the error and return an empty string
-            System.err.println("Invalid token format: " + token);
+            var subject = JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+            return subject;
+        } catch (JWTVerificationException ex) {
+            ex.printStackTrace();
             return "";
         }
     }
